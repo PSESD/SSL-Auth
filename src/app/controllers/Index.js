21022c0543e9fc78c8c3efcb90e239b9168d0bd7
zaenal <zaenal@upwardstech.com>
2015-05-26 20:55:40 +0700

    add welcome page

---
+++ src/app/controllers/Index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zaenal on 20/05/15.
+ */
+var Index = {
+
+    index: function(req, res){
+        res.json({ "test": "Welcome To Auth Server"});
+    }
+};
+
+module.exports = Index;
